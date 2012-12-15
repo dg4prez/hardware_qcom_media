be1350067e7258e89936ce4616063551b06e7cd9
Stevespear426 <stephen.k.spear@gmail.com>
2012-12-15 18:19:11 +0100

    dont use strict fix omx_vdec_test error
    Change-Id: I1e25d9150c0537983deb6028b9a420e1bf702297
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

--- mm-video/vidc/vdec/Android.mk
+++ mm-video/vidc/vdec/Android.mk
@@ -101,6 +101,7 @@ mm-vdec-test-inc    += $(LOCAL_PATH)/inc
 LOCAL_MODULE                    := mm-vdec-omx-test
 LOCAL_MODULE_TAGS               := optional
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
+LOCAL_CFLAGS                    += -fno-strict-aliasing
 LOCAL_C_INCLUDES                := $(mm-vdec-test-inc)
 
 LOCAL_PRELINK_MODULE      := false
@@ -122,6 +123,7 @@ mm-vdec-drv-test-inc    += $(LOCAL_PATH)/inc
 LOCAL_MODULE                    := mm-video-driver-test
 LOCAL_MODULE_TAGS               := optional
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
+LOCAL_CFLAGS                    += -fno-strict-aliasing
 LOCAL_C_INCLUDES                := $(mm-vdec-drv-test-inc)
 LOCAL_PRELINK_MODULE            := false
 
